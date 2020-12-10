d737c77e817840b4bcae180a94debdee03b665ab
Evelinn <evelinn.idenfors@gmail.com>
2020-12-10 13:27:36 +0100

    updated site

---
+++ index.js
@@ -0,0 +1,11 @@
+//const checkpoint = 500;
+ 
+//window.addEventListener("scroll", () => {
+//  const currentScroll = window.pageYOffset;
+//  if (currentScroll <= checkpoint) {
+//    opacity = 0 - currentScroll / checkpoint;
+//  } else {
+//    opacity = 0;
+//  }
+//  document.querySelector(".box-style").style.opacity = opacity;
+//});
